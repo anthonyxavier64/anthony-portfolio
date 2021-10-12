9214b98951439076058eaf524dcd462066a2954c
anthonyxavier64 <e0406854@u.nus.edu>
2021-10-12 20:28:59 +0800

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './pages/index/index.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MglTimelineModule } from 'angular-mgl-timeline';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +18,12 @@ import { IndexComponent } from './pages/index/index.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MglTimelineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  nusDate: Date;
+  time: string;
 
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller) {
+    this.nusDate = new Date('August, 2019');
+    this.time="";
+  }
 
   ngOnInit(): void {
   }
 
+  scrollIntoView(elementId: string): void {
+    document.getElementById(elementId)?.scrollIntoView({behavior: "smooth"});
+  }
+
+  handleDownloadResume() {
+    window.open("https://firebasestorage.googleapis.com/v0/b/anthony-portfolio-d41e3.appspot.com/o/Resume_Anthony%20Xavier%20Poh%20TIanci.pdf?alt=media&token=bc3676bf-71e1-4d40-89aa-a307cad84943", "_blank");
+  }
+
+  handleKollabpalTestimonial() {
+    window.open("https://firebasestorage.googleapis.com/v0/b/anthony-portfolio-d41e3.appspot.com/o/Kollabpal%20testimonial_Anthony%20Xavier%20Poh%20Tianci.pdf?alt=media&token=b7eb7553-816d-4a93-b395-2337e68a2e26", "_blank");
+  }
+
+  handleHackAndRoll() {
+    window.open("https://github.com/ccwhgetgit/hacknroll2021", "_blank");
+  }
+
+  handlePMFrontend() {
+    window.open("https://github.com/anthonyxavier64/PerfectMatchAngular", "_blank");
+  }
+
+  handlePMBackend() {
+    window.open("https://github.com/anthonyxavier64/perfectMatch", "_blank");
+  }
+
+  handlePerFit() {
+    window.open("https://github.com/kkangs0226/PerFit", "_blank");
+  }
+
+  handleMerlionAirlines() {
+    window.open("https://github.com/anthonyxavier64/MerlionAirlines", "_blank");
+  }
+
+  handleNESinsta() {
+    window.open("https://www.instagram.com/nusentresoc/?hl=en", "_blank");
+  }
+
+  handleNESfb() {
+    window.open("https://www.facebook.com/nusentresoc/", "_blank");
+  }
+
+  handleComClubinsta() {
+    window.open("https://www.instagram.com/nuscomputingclub/?hl=en", "_blank");
+  }
+
+  handleComClubfb() {
+    window.open("https://www.facebook.com/nuscomputing/", "_blank");
+  }
 }
